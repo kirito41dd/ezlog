85a89015a73791e9e192631c94b685f13764585e
zsh <1026860069@qq.com>
2020-02-14 12:52:23 +0800

    initialization

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zshorz/ezlog
+
+go 1.13
---
+++ version.go
@@ -0,0 +1,13 @@
+/*
+ * Author: zsh
+ * Create: 2020/2/14
+ */
+
+package ezlog
+
+const version string = "v0.1"
+
+// return version as string
+func Version() string {
+	return version
+}
